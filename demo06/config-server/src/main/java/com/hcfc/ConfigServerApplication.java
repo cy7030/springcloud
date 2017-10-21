e9d9401d864009fbbb2107f93c0c2ac86c4f5b46
陈勇 <32617014+cy7030@users.noreply.github.com>
2017-10-21 22:09:28 +0800

    commit

--- demo06/config-server/src/main/java/com/hcfc/ConfigServerApplication.java
+++ demo06/config-server/src/main/java/com/hcfc/ConfigServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer//开启配置服务器的功能， 使用Spring cloud config 分布式配置中心组件
+@EnableConfigServer //开启配置服务器的功能， 使用Spring cloud config 分布式配置中心组件
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
