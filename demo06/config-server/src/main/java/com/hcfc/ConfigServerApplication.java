b2eaa16b1845da129a3ea5c1073d48c3c437b1e8
陈勇 <32617014+cy7030@users.noreply.github.com>
2017-10-23 14:01:23 +0800

    配置高可用的分布式配置中心

--- demo06/config-server/src/main/java/com/hcfc/ConfigServerApplication.java
+++ demo06/config-server/src/main/java/com/hcfc/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.hcfc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer//开启配置服务器的功能， 使用Spring cloud config 分布式配置中心组件
+@EnableEurekaClient //声明eureka的客户端
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
