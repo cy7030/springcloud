44c63e951d66ec565476aefe5ea35709d255deaf
chenyong <568669969@qq.com>
2018-05-30 10:20:50 +0800

    commit

--- demo03/serviceRibbon/src/test/java/com/yong/serviceribbon/ServiceRibbonApplicationTests.java
+++ demo03/serviceRibbon/src/test/java/com/yong/serviceribbon/ServiceRibbonApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hcfc.sericefeign;
+package com.yong.serviceribbon;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SericeFeignApplicationTests {
+public class ServiceRibbonApplicationTests {
 
 	@Test
 	public void contextLoads() {
